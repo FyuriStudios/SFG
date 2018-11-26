c7686bf84565f4fe7eae035985308404d3d6e646
s024000 <s024000@lm-19s024000.fios-router.home>
2018-11-26 18:33:01 -0500

    Started to add backend

--- SFG - 2/base/Game.js
+++ SFG - 2/base/Game.js
@@ -5,27 +5,29 @@ class Game {
     constructor(socket1, socket2) {
 	this.turnCounter = 0
 	//this.eventHistory = []
-	
+
 	/*
 	 Here, I'm creating non-prototypical objects for player1 and player2. They don't need any functions, just some variables.
 	 These player objects contain the exact same variable names, so it's super easy to just ask for something like currentPlayer().board
 	 or something like that.
 	 */
 	this.player1 = {
+		id: 1,
 		socket: socket1,
 		board: [],
 		hand: [],
 		graveyard: []
 	}
-	
+
 	this.player2 = {
+		id: 2,
 		socket: socket2,
 		board: [],
 		hand: [],
 		graveyard: []
 	}
     }
-    
+
     /**
      * After creating a new Game object, call this function. It will run an entire game from start to finish.
      * I didn't make this part of the constructor in case we decide that we want to create a game but wait to start it.
@@ -33,13 +35,24 @@ class Game {
     start() {
 	player1.board = []//TODO: add a default deck
 	player2.board = []
-	player1.socket.on
+	
+	var doTurn = function(input) {
+	    if(input.player != this.currentPlayer) {
+		return //If we receive input from the other player, ignore it.
+	    } else {
+		
+	    }
+	}
+
+	player1.socket.on('move', function(input) {
+
+	})
     }
 
 
-//    get allEffects() {
-//	return player1.board.concat(player1.effects).concat(player1)
-//    } /* I'm commenting this out because we aren't implementing effects yet. */
+//  get allEffects() {
+//  return player1.board.concat(player1.effects).concat(player1)
+//  } /* I'm commenting this out because we aren't implementing effects yet. */
 
     get currentPlayer() {
 	return (turnCounter%4 == 1 || turnCounter%4 == 2) ? player1:player2
@@ -49,6 +62,7 @@ class Game {
 
     get otherPlayer() {
 	return (turnCounter%4 == 1 || turnCounter%4 == 2) ? player2:player1
+		//Just the opposite of currentPlayer.
     }
 
     endTurn() {
@@ -60,13 +74,13 @@ class Game {
 //	}
 	killDead()
     }
-    
+
     startTurn() {
 //	eventHistory.push(new TurnBeginsEvent(this))
 	turnCounter++
 //	for(element in effects){
-//	    if(element.hasTurnIncrement())
-//		element.turnIncrement()
+//	if(element.hasTurnIncrement())
+//	element.turnIncrement()
 //	}
 	killDead()
     }
@@ -80,3 +94,5 @@ class Game {
     }
 
 }
+
+module.exports = Game
