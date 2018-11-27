e1aff1090002ed84299c4c7885d466f5ebc7089f
s024000 <s024000@lm-19s024000.fios-router.home>
2018-11-26 20:50:46 -0500

    fixes round the board

--- base/Server.js
+++ base/Server.js
@@ -1,3 +1,7 @@
+/**
+ * This file sets up the entire project. You should run this file from your server, it will make everything work like magic.
+ */
+
 //Import all needed components
 var express = require('express')
 var http = require('http')
@@ -16,15 +20,19 @@ const port = 6001
 
 app.set('port', port)
 
-//This line of code sets the "Static" directory for the webapp: That is, the outward-facing chunk of this project. When I launch this app and someone connects to it,
-//they'll be able to access the directory named 'static' as specified below.
-//__dirname is a global variable that is just the current directory that this file is living in. 
-//'/static' is appended to it to point to the [current]/static directory.
+/*
+This line of code sets the "Static" directory for the webapp: That is, the outward-facing chunk of this project. When I launch this app and someone connects to it,
+they'll be able to access the directory named 'static' as specified below.
+__dirname is a global variable that is just the current directory that this file is living in. 
+'/static' is appended to it to point to the [current]/static directory.
+*/
 app.use('/static', express.static(__dirname + '/static'))
 
-//This runs while the server is running and gives the root file every time that someone connects to the server. By default, when connecting to a server your computer will ask for the
-//root directory of the server ('/'). This will respond by giving back the path to the index.html file, which is the file that gets loaded by default on connection from 
-//the user.
+/*
+This runs while the server is running and gives the root file every time that someone connects to the server. By default, when connecting to a server your computer will ask for the
+root directory of the server ('/'). This will respond by giving back the path to the index.html file, which is the file that gets loaded by default on connection from 
+the user.
+*/
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'static/index.html'))//I'm leaving it like this for now. But we can totally change it.
 })
@@ -34,15 +42,18 @@ server.listen(port, function() {
     console.log('Server is running on port ' + port)
 })
 
-//This variable is sitting here so that I can hold one extra socket reference inside it.
-//Since we need two players to play the game, I'll keep a second reference while I'm waiting for two players. 
-//This will get overwritten when I have both players and I can place them into a game.
-
+/*
+This variable is sitting here so that I can hold one extra socket reference inside it.
+Since we need two players to play the game, I'll keep a second reference while I'm waiting for two players. 
+This will get overwritten when I have both players and I can place them into a game.
+*/
 //FIXME: We should probably find a better way to do this.
 var playerStorage = null
 
-//This sets the in-out library to listen for a person connecting to the server. This function gives a reference to the socket that they're connecting through,
-//and we'll use that to create an instance of our Game.
+/*
+This sets the in-out library to listen for a person connecting to the server. This function gives a reference to the socket that they're connecting through,
+and we'll use that to create an instance of our Game.
+*/
 io.on('connection', function(playerSocket) {
 
     if(playerStorage == null) {
@@ -53,6 +64,7 @@ io.on('connection', function(playerSocket) {
     else {
 	//Otherwise, we'll create a new game with both players and run it.
 	var g = new Game(playerStorage, playerSocket).start()
+	playerStorage = null
 	//I don't think that this file needs anything else in it; the Game is meant to handle the rest of everything.
     }
 })
