6f702de39db4d7e6924e15421f99f630e32b7dbb
s024000 <s024000@lm-19s024000.lmsd.org>
2018-12-07 10:13:51 -0500

    test commit (also, started writing frontend)

--- static/game.js
+++ static/game.js
@@ -1,24 +1,26 @@
-var stage = new createjs.Stage("canvas")
-var socket = io()
+var canvas = new createjs.Stage("canvas")
+//var socket = io()
+//
+//var playerID
+//
+//var self
+//
+//var other
+//
+//var turnCounter
+//
+//socket.on('player id', (id) => {playerID = id})
+//
+//socket.on('game state', (input) => {
+//    self = input.self
+//    other = input.other
+//    turnCounter = input.gameCounter
+//    gameUpdate()
+//})
+//
+//function gameUpdate() {
+//    
+//}
 
-var playerID
-
-var self
-
-var other
-
-var turnCounter
-
-socket.on('player id', (id) => {playerID = id})
-
-socket.on('game state', (input) => {
-    self = input.self
-    other = input.other
-    turnCounter = input.gameCounter
-    gameUpdate()
-})
-
-function gameUpdate() {
-    
-}
-
+canvas.height = window.innerHeight()
+canvas.width = window.innderWidth()
