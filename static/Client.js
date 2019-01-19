fea0d984f0b792edfc5df6f68506337363464b5e
Seen <44823666+SeenTheShadow@users.noreply.github.com>
2019-01-18 21:29:17 -0500

    A zero now displays, I repeat, A zero now displays.

--- static/Client.js
+++ static/Client.js
@@ -6,10 +6,10 @@ const offset = 20;
 
 var grass = 2;
 
-//creates a PIXI Application to draw stuff on 
+//creates a PIXI Application to draw stuff on
 //renderer, ticker, stage aka container is automatically created with app
 //only create this once, the app is mutated in resizeCanvas and only drawn to the screen in func drawboard
-//I'm not sure if the redrawn board is a new node added to the end of the list though. Children pile?? :( 
+//I'm not sure if the redrawn board is a new node added to the end of the list though. Children pile?? :(
 let app = new PIXI.Application({
     antialias: true,    // default: false
     transparent: true, // default: false
@@ -28,9 +28,9 @@ function resizeCanvas() {
 
     //now obsolete but in case you still want the random rectangle
     // var canvas = document.getElementById('canvas');
-    
 
-   
+
+
     //I subtracted 20 so you can now see the outside lines of the board when you're resizing. Open to changes in offset.
     if (innerWidth * aspectRatio  <= innerHeight) {
         app.renderer.resize(innerWidth - offset, innerWidth*aspectRatio - offset);
@@ -46,16 +46,16 @@ function resizeCanvas() {
 }
 
 function init() {
-    
+
 //    var socket = io(); //initialize the socket connection
 //    socket.emit('connected', {}); //TODO: input your deck and hero here
-//    
+//
 //    socket.on('init', function(info) {
 //	 game = new Game(info.thisPlayer, info.otherPlayer);//TODO: we need a fancy game starting animation here, also I don't think this import works
 //    });
-//    
-//    socket.on('update', function(info) { 
-//	
+//
+//    socket.on('update', function(info) {
+//
 //    });
 
   //initialize game objects here
@@ -68,10 +68,10 @@ function init() {
 
     let loader = PIXI.loader;
 
-    loader.add('./assets/numbers/Power-00.png');
+    loader.add('/static/assets/numbers/Power-00.png');
 
     loader.load(function(loader, resources) {
-        board = PIXI.Sprite.fromImage('./assets/numbers/Power-00.png');
+        board = PIXI.Sprite.fromImage('/static/assets/numbers/Power-00.png');
     });
 
     loader.onComplete.add(function() {
