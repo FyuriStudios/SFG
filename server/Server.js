d557450ae03ebb0e3d09e5967947a10199af0d49
Hughes <hugslondergan@gmail.com>
2019-01-20 00:04:47 -0500

    Fix Server.js for testing

--- server/Server.js
+++ server/Server.js
@@ -3,21 +3,20 @@
  */
 
 //Import all needed components
-var express = require('express')
-var http = require('http')
-var path = require('path')
-var socketIO = require('socket.io')
-var createjs = require('create.js')
-var Game = require('./Game.js')
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
+var Game = require('./Game.js');
 //Create the server and instances of all the packages we'll need.
-var app = express()
-var server = http.Server(app)
-var io = socketIO(server)
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
 
 //The port will remain set to 6001 for now; that's the port that I've reserved on my router.
-const port = 6001
+const port = 6001;
 
-app.set('port', port)
+app.set('port', port);
 
 /*
 This line of code sets the "Static" directory for the webapp: That is, the outward-facing chunk of this project. When I launch this app and someone connects to it,
@@ -32,12 +31,12 @@ root directory of the server ('/'). This will respond by giving back the path to
 the user.
 */
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../static/index.html'))//I'm leaving it like this for now. But we can totally change it.
+    res.sendFile(path.join(__dirname, '../static/index.html'));//I'm leaving it like this for now. But we can totally change it.
 })
 
-//Set the server to listen on port 4000 for a webapp running. When the webapp is running, it calls the function below that simply outputs to the console that the application is working.
+//Set the server to listen on port 6001 for a webapp running. When the webapp is running, it calls the function below that simply outputs to the console that the application is working.
 server.listen(port, function() {
-    console.log('Server is running on port ' + port)
+    console.log('Server is running on port ' + port);
 })
 
 /*
@@ -46,7 +45,8 @@ Since we need two players to play the game, I'll keep a second reference while I
 This will get overwritten when I have both players and I can place them into a game.
 */
 //FIXME: We should probably find a better way to do this.
-var playerStorage = null
+var playerStorage = null;
+var games = [];//so we can reference the games if we care
 
 /*
 This sets the in-out library to listen for a person connecting to the server. This function gives a reference to the socket that they're connecting through,
@@ -55,14 +55,14 @@ and we'll use that to create an instance of our Game.
 io.on('connection', function(playerSocket) {
 
     if(playerStorage == null) {
-	//Keep a reference to the socket so that it doesn't get dereferenced while we wait for another player.
-	playerStorage = playerSocket
+	    //Keep a reference to the socket so that it doesn't get dereferenced while we wait for another player.
+	    playerStorage = playerSocket;
     }
 
     else {
-	//Otherwise, we'll create a new game with both players and run it.
-	var g = new Game(playerStorage, playerSocket).start()
-	playerStorage = null
-	//I don't think that this file needs anything else in it; the Game is meant to handle the rest of everything.
+	    //Otherwise, we'll create a new game with both players and run it.
+	    games.push(new Game(playerStorage, playerSocket).start());
+	    playerStorage = null;
+	    //I don't think that this file needs anything else in it; the Game is meant to handle the rest of everything.
     }
 })
