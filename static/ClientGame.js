8392e373dcf8ec9d306522cf31a98b920a5d01e7
Hughes <hugslondergan@gmail.com>
2019-01-28 10:31:22 -0500

    Add framework for work to come

--- static/ClientGame.js
+++ static/ClientGame.js
@@ -1,33 +1,15 @@
-var socket = io();
+import ClientGameDisplay from './ClientGameDisplay';
 var constants = require('../sharedConstants/constants');
 
 
 /**
  * This file holds all of the information required for the frontend to display the game properly.
- * It also contains the event history needed to do ~fancy~ animations (or it will, TODO: get events working)
- * 
+ * It also contains the event history needed to do fancy animations (or it will, TODO: get events working)
  */
 export default class ClientGame { //FIXME: I'm pretty sure this export line doesn't actually work
     
     constructor() {
-		this.setUp = false;
-	}
-
-	start() {
-		socket.on('player id', playerIDCallback);
-		socket.on('starting hand', startingHandCallback);
-	}
-	
-	playerIDCallback(input) {
-		this.ownPlayer = {
-			id: input,
-			characterHealth: PLAYER_MAX_HEALTH,
-		};
-		socket.emit('deck', null);
-	}
-
-	startingHandCallback(input) {
-		this.ownPlayer.hand = input.cards;
+		this.view = new ClientGameDisplay(/* params here */);
 	}
 	
 }
---
+++ static/ClientGameDisplay.js
@@ -0,0 +1,26 @@
+/**
+ * I created this file as the hopeful new location for the code that displays the frontend and handles user input so that it can be
+ * sent to the frontend. Sean, if you have any issues with this just lmk. There's not much here right now but I've just put in a very
+ * small shell for what's actually going on here.
+ * 
+ * This class should handle the graphics of the game, much like Client.js is doing at this very moment (Jan 28).
+ */
+
+export default class ClientGameDisplay {
+
+    /**
+     * TODO: put actual things here in this constructor.
+     * @param {*} params 
+     */
+    constructor(params) {
+        
+    }
+
+    /**
+     * This function will be called from Frontend.js every time that the window resizes. We should just migrate the current code from
+     * Client.js into this file.
+     */
+    windowResize() {
+
+    }
+}
