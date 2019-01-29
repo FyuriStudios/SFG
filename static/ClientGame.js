b5331693a45249bad48d24a7efde19ce6fd563db
Hughes <hugslondergan@gmail.com>
2019-01-28 23:22:07 -0500

    Add processEvent to the ClientGame class

--- static/ClientGame.js
+++ static/ClientGame.js
@@ -12,6 +12,21 @@ export default class ClientGame { //FIXME: I'm pretty sure this export line does
 		this.view = new ClientGameDisplay(/* params here */);
 		this.ownPlayer = ownPlayer;
 		this.otherPlayer = otherPlayer;
+		this.turnCounter = turnCounter;
+		this.history = [];//this contains the full event history.
+	}
+
+	/**
+	 * This is the most important function of the class. It takes in an event and then turns the event into
+	 * a graphical change for the user and updates its own data based on the event's information. The documentation
+	 * on event formatting is at the top of Game.js. This event processor will probably get broken down into a bunch of methods
+	 * so that it's easier to read.
+	 * @param {*} event the event to be processed
+	 */
+	processEvent(event) {
+		if(event.type == 'draw card') {
+			processDrawCardEvent(event); //like dat
+		}
 	}
 	
 }
