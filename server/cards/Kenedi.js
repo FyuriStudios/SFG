00597e1fabe6780086efdee00578da344cf70475
Mekkx <wackymacky54@gmail.com>
2019-02-12 14:07:53 -0500

    Rewrote Effect so we can add effects externally

--- server/cards/Kenedi.js
+++ server/cards/Kenedi.js
@@ -5,30 +5,35 @@ class Kenedi extends Monster {
     
     constructor() {
         super('monster', 2, 'monster', 'legendary', 8, 2);
+        this.hasCardPlayed = true;
+
+        this.addCardPlayed({
+            name: "Kenedi Heal",
+            func: function (noInput, game, eventChain) {
+                let event = {
+                    type: 'heal',
+                    player: game.currentPlayer.id,
+                    targetSide: game.currentPlayer.id,
+                    target: -1,//the hero
+                };
+        
+                if(game.currentPlayer.health > constants.STARTING_HEALTH-20) {
+                    event.amount = constants.STARTING_HEALTH-game.currentPlayer.health;
+                    game.currentPlayer.health = constants.STARTING_HEALTH;
+                } else {
+                    event.amount = 20;
+                    game.currentPlayer.health += 20;
+                }
+        
+                eventChain.push(event);
+            }
+        });
     }
 
     get hasCardPlayed() {
         return true;
     }
 
-    cardPlayed(noInput, game, eventChain) {
-        let event = {
-            type: 'heal',
-            player: game.currentPlayer.id,
-            targetSide: game.currentPlayer.id,
-            target: -1,//the hero
-        };
-
-        if(game.currentPlayer.health > constants.STARTING_HEALTH-20) {
-            event.amount = constants.STARTING_HEALTH-game.currentPlayer.health;
-            game.currentPlayer.health = constants.STARTING_HEALTH;
-        } else {
-            event.amount = 20;
-            game.currentPlayer.health += 20;
-        }
-
-        eventChain.push(event);
-    }
 
 }
 
