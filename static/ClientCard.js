776ba76f7f74b107843a885bdbb061fb667edd24
Hughes <hugslondergan@gmail.com>
2019-03-01 13:59:37 -0500

    Modularize ClientCard

--- static/ClientCard.js
+++ static/ClientCard.js
@@ -5,57 +5,68 @@ Stuff that this file might be missing:
 */
 
 /**
- * Defines a generic card. This class shouldn't be constructed directly.
+ * Turned ClientCard into a module as well.
  */
-class ClientCard {
-    constructor(type, id, tokenType, rarity, name, cost) {
-    	this.type = type;
-    	this.id = id;
-    	this.tokenType = tokenType;
-    	this.rarity = rarity;
-    	this.name = name;
-    	this.cost = cost;
-        this.currentCost = cost;//current cost and absolute cost are different
-        this.sprite = PIXI.Sprite.from(AssetGetter.getCardAsset(id));
-    }
-}
-/**
- * Defines a generic monster.
- */
-class Monster extends Card {
-    constructor(backendCard) {
-        super(backendCard.type, backendCard.id, backendCard.tokenType, backendCard.rarity, backendCard.cost, backendCard.power, backendCard.hasDefender);
-        this.power = backendCard.power;
-        this.currentPower = this.power;
-        this.hasDefender = backendCard.hasDefender;
-        this.isStatic = backendCard.isStatic;
-    }
-}
+ClientCard = (function() {
 
-/**
- * Defines a generic spell.
- */
-class Spell extends Card {
-    constructor(backendCard) {
-        super(backendCard.type, backendCard.id, backendCard.tokenType, backendCard.rarity, backendCard.cost, backendCard.power, backendCard.hasDefender);
-    }
-}
+    /**
+     * Defines a generic card. This class shouldn't be constructed directly.
+     */
+    class Card {
+        constructor(type, id, tokenType, rarity, name, cost) {
+            this.type = type;
+            this.id = id;
+            this.tokenType = tokenType;
+            this.rarity = rarity;
+            this.name = name;
+            this.cost = cost;
+            this.currentCost = cost;//current cost and absolute cost are different
+            this.sprite = PIXI.Sprite.from(AssetGetter.getCardAsset(id));
+        }
 
-/**
- * This is the outward facing function of the file. It takes in a card object from the backend and adds all of its information plus
- * more information that might be useful to have on the frontend into one big Card object, like a Sprite for the card, then returns it.
- * @param {Card} backendCard
- */
-export function from(backendCard) {
-    if(backendCard.type == 'monster') {
-        return new Monster(backendCard);
+    }
+    /**
+     * Defines a generic monster.
+     */
+    class Monster extends Card {
+        constructor(backendCard) {
+            super(backendCard.type, backendCard.id, backendCard.tokenType, backendCard.rarity, backendCard.cost, backendCard.power, backendCard.hasDefender);
+            this.power = backendCard.power;
+            this.currentPower = this.power;
+            this.hasDefender = backendCard.hasDefender;
+            this.isStatic = backendCard.isStatic;
+        }
     }
 
-    else if(backendCard.type == 'spell') {
-        return new Spell(backendCard);
+    /**
+     * Defines a generic spell.
+     */
+    class Spell extends Card {
+        constructor(backendCard) {
+            super(backendCard.type, backendCard.id, backendCard.tokenType, backendCard.rarity, backendCard.cost, backendCard.power, backendCard.hasDefender);
+        }
     }
 
-    else {
-        throw('invalid monster type');
-    }
-}
+    return {
+            /**
+         * This is the outward facing function of the file. It takes in a card object from the backend and adds all of its information plus
+         * more information that might be useful to have on the frontend into one big Card object, like a Sprite for the card, then returns it.
+         * @param {Card} backendCard
+         */
+        from: function(backendCard) {
+            if(backendCard.type == 'monster') {
+                return new Monster(backendCard);
+            }
+
+            else if(backendCard.type == 'spell') {
+                return new Spell(backendCard);
+            }
+
+            else {
+                throw('invalid monster type');
+            }
+        }
+    };
+})();
+
+
