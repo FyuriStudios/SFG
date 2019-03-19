edc63886a05258d0ed1d1216ad181e3733dbb988
Hughes <hugslondergan@gmail.com>
2019-03-19 08:46:08 -0400

    Delete unused files

--- static/FrontendTester.js
+++ static/FrontendTester.js
@@ -1,10 +1,16 @@
+
+/*
+This starts a test script on the view that draws one card and pipes user output through standard out rather than through a socket 
+connection. We're using this for now until the entire frontend functionality is finished and we can connect to the backend.
+*/
 let test = function() {
+
     GameView.setupDisplay();
 
     GameView.setupOutput(function(output) {
         console.log(output);
     });
     
-    setTimeout(() => {GameView.processEvent({type: 'draw card', player: 1});}, 1000)
+    setTimeout(() => {GameView.processEvent({type: 'draw card', player: 1});}, 1000);
     
 };
