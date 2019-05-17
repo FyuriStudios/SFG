efd9fe9dc4d34c08ca7106efa70cba0430bdee6c
Hughes <hugslondergan@gmail.com>
2019-05-17 13:33:59 -0400

    Add forgotten paths for Melody and Mantra

--- server/IDToCard.js
+++ server/IDToCard.js
@@ -8,4 +8,12 @@ module.exports = function (id) {
         let card = require('./cards/test2');
         return new card();
     }
+    else if(id == 1) {
+        let card = require('./cards/Melody');
+        return new card();
+    }
+    else if(id == 2) {
+        let card = require('./cards/Mantra');
+        return new card();
+    }
 }
