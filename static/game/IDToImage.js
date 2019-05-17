8ac3116d32ade44f9059c4c98e3579de5b6dccfd
Hughes <hugslondergan@gmail.com>
2019-05-17 12:35:34 -0400

    Fix file paths

--- static/game/IDToImage.js
+++ static/game/IDToImage.js
@@ -4,25 +4,23 @@ IDToImage = (function () {
 
         fromID: function(id) {
             if(id == -1) {
-                return PIXI.Sprite.from('/static/assets/cards/Hickory-Test.png');
+                return PIXI.Sprite.from('/static/assets/in_hand/Hickory-Test.png');
             } else if(id == -2) {
-                return PIXI.Sprite.from('/static/assets/cards/test2.png');
+                return PIXI.Sprite.from('/static/assets/in_hand/test2.png');
             }
-            return PIXI.Sprite.from('/static/assets/cards/Hickory-Test.png');
         },
 
         popupFromId: function(id) {
             if(id == -1) {
-                return PIXI.Sprite.from('/static/assets/cards/test_hover.png');
+                return PIXI.Sprite.from('/static/assets/popup/test_popup.png');
             } else if(id == -2) {
-                return PIXI.Sprite.from('/static/assets/cards/test2_popup.png');
+                return PIXI.Sprite.from('/static/assets/popup/test2_popup.png');
             }
-            return PIXI.Sprite.from('/static/assets/cards/test_hover.png');
         },
 
         monsterFromID: function(id) {
             if(id == -1) {
-                return PIXI.Sprite.from('/static/assets/monster_pieces/Hickory_Board_Piece.png');
+                return PIXI.Sprite.from('/static/assets/board_pieces/Hickory_Board_Piece.png');
             }
         }
     }
