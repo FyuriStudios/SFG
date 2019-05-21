7734fd38da5180dacd4ddeedfefdb9b93cb3e967
Hughes <hugslondergan@gmail.com>
2019-05-21 10:57:39 -0400

    Change Melody's effect until after attack happens

--- server/cards/Melody.js
+++ server/cards/Melody.js
@@ -54,83 +54,32 @@ class Melody extends Monster {
                 }
             }
         });
+
+        this.hasSelfAttack = true;
+
+        let temp = this;
+
+        this.addSelfAttack({
+            name: 'Melody Attack',
+            func: function(input, game, eventChain) {
+                temp.currentPower += 2;
+
+                let melodyIndex;
+                game.currentPlayer.board.forEach((value, index) => value == temp? melodyIndex = index:null);
+
+                let boostEvent = {
+                    type: 'boost',
+                    targetSide: game.currentPlayer.id,
+                    target: melodyIndex,
+                    boost: 2
+                };
+
+                eventChain.push(boostEvent);
+            }
+        });
  
     }
 
-    attack(game, attackerLoc, targetLoc, eventChain) {
-
-        if (this.turnsBeforeAttack > 0) {
-			return false;
-		}
-
-		if (this.hasDefender || this.isStatic) //first, we'll check the obvious and make sure that the attacking monster isn't a defender or disabled.
-			return false;
-
-		let defenders = [];
-
-		game.otherPlayer.board.forEach((monster, location) => {
-			if (monster.hasDefender) {
-				defenders.push(location);
-			}
-		});
-
-		if (defenders.length > 0 && !defenders.includes(targetLoc))
-            return false;
-
-        if(targetLoc == -1) {
-            let melodyIndex;
-            game.currentPlayer.board.forEach((value, index) => {
-                if(value == this) {
-                    melodyIndex = index;
-                }
-            });
-
-            this.currentPower += 2;
-
-            let boostEvent = {
-                type: 'boost',
-                targetSide: game.currentPlayer.id,
-                target: melodyIndex,
-                boost: 2
-            };
-
-            eventChain.push(boostEvent);
-        }
-
-        var event = {
-            type: 'attack',
-            player: game.currentPlayer.id,
-            attacker: attackerLoc,
-            target: targetLoc,
-        }
-            
-        if (targetLoc == -1) {
-            let currHealth = game.otherPlayer.health;
-            game.otherPlayer.health -= this.currentPower;
-            event.damageToDefender = currHealth - game.otherPlayer.health;
-            event.damageToAttacker = 0;
-        }
-        else {
-            let tempAPower = this.currentPower;
-            let tempTPower = game.otherPlayer.board[targetLoc].currentPower;
-
-            this.currentPower -= tempTPower;
-            game.otherPlayer.board[targetLoc].currentPower -= tempAPower;
-
-            event.damageToDefender = tempTPower - game.otherPlayer.board[targetLoc].currentPower;
-            event.damageToAttacker =  tempAPower - this.currentPower;
-        }
-
-		eventChain.push(event);
-
-		if (this.relentless)
-			this.turnsBeforeAttack = 1;
-		else
-			this.turnsBeforeAttack = 2;
-
-		return true;
-        
-    }
 
 }
 
