e90a0cfcc7a4b040fdb2308f3e972e8be041a98e
Mekkx <wackymacky54@gmail.com>
2019-05-29 22:38:01 -0400

    Add Zhaori

---
+++ server/cards/Zhaori.js
@@ -0,0 +1,10 @@
+let Monster = require('../Monster');
+
+class Zhaori extends Monster {
+
+    constructor() {
+        super('monster', 27, 'monster', 'uncommon', 'Zhaori the Bygone', 5, 8, 'cur', false, false, true, false, false)
+    }
+}
+
+module.exports = Zhaori;
