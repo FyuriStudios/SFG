e7ff2c533d8f2e9d45650ba1fb5774c227108ef8
Hughes <hugslondergan@gmail.com>
2019-05-29 23:57:46 -0400

    Fix Max's doofusness, again

--- server/cards/Poke.js
+++ server/cards/Poke.js
@@ -12,6 +12,7 @@ class Poke extends Monster {
            name: 'untitled',
            func: function(input, game, eventChain) {
                card.creationDate = game.turnCounter;
+               card.id = game.otherPlayer.id;
            } 
         });
 
@@ -19,7 +20,7 @@ class Poke extends Monster {
         this.addTurnIncrement({
             name: 'Poke Turn Increment',
             func: function(input, game, eventChain) {
-                input.targetSide = game.otherPlayer.id;
+                input.targetSide = card.id;
                 input.target = -1;
                 if(game.turnCounter%2 == (card.creationDate + 1)%2) {
                     Damage.func(input, game, eventChain, 1);
