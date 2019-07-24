81bbd71cd933631fbbde4aa3cd116646ffc60f14
Hughes <hugslondergan@gmail.com>
2019-07-24 16:12:42 -0400

    Fix Rocket Rain

--- server/cards/RocketRain.js
+++ server/cards/RocketRain.js
@@ -5,12 +5,15 @@ class RocketRain extends Spell {
     constructor() {
         super(39, 'spell', 'common', 'Rocket Rain', 5, false, false, false);
         this.hasCardPlayed = true;
-        this.addCardPlayed(function(input, game, eventChain){
-            let damage = require('../genericEffects/Damage');
-            damage.func({
-                targetSide: game.otherPlayer.id,
-                target: -1
-            }, game, eventChain, 9);
+        this.addCardPlayed({
+            name: 'Rocket Rain',
+            func: function(input, game, eventChain){
+                let damage = require('../genericEffects/Damage');
+                damage.func({
+                    targetSide: game.otherPlayer.id,
+                    target: -1
+                }, game, eventChain, 9);
+            }
         });
     }
 
