b9b419d5875b3204b5c09eba77e4465e603f3f67
Hughes <hugslondergan@gmail.com>
2019-07-24 20:59:19 -0400

    Fix Ishiguro's

--- server/cards/IshigurosGuidance.js
+++ server/cards/IshigurosGuidance.js
@@ -5,24 +5,26 @@ class Ishiguro extends Spell {
     constructor() {
         super(38, 'spell', 'uncommon', "Ishiguro's Guidance", 3, false, false, false)
         this.hasCardPlayed = true;
-        this.addCardPlayed(function(input, game, eventChain){
-            for(let i = game.currentPlayer.deck.length-1; i>=0; i--){
-                if (game.currentPlayer.deck[i].type == 'monster'){
-                    game.currentPlayer.deck.push(game.currentPlayer.deck.splice(i, 1)[0]);
-                    game.drawCard(game.currentPlayer, eventChain);
-                    break;
+        this.addCardPlayed({
+            name: 'Ishiguro',
+            func: function(input, game, eventChain){
+                for(let i = game.currentPlayer.deck.length-1; i>=0; i--){
+                    if (game.currentPlayer.deck[i].type == 'monster'){
+                        game.currentPlayer.deck.push(game.currentPlayer.deck.splice(i, 1)[0]);
+                        game.drawCard(game.currentPlayer, eventChain);
+                        break;
+                    }
                 }
-            }
-            
-            for(let i = game.currentPlayer.deck.length-1; i>=0; i--){
-                if (game.currentPlayer.deck[i].type == 'spell'){
-                    game.currentPlayer.deck.push(game.currentPlayer.deck.splice(i, 1)[0]);
-                    game.drawCard(game.currentPlayer, eventChain);
-                    break;
+                
+                for(let i = game.currentPlayer.deck.length-1; i>=0; i--){
+                    if (game.currentPlayer.deck[i].type == 'spell'){
+                        game.currentPlayer.deck.push(game.currentPlayer.deck.splice(i, 1)[0]);
+                        game.drawCard(game.currentPlayer, eventChain);
+                        break;
+                    }
+                
                 }
-            
-            }
-            
+            }    
         });
     }
 
