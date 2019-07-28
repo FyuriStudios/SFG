01fe51d6edef4417c3f39f17f82f55ac4a6ef2c6
Hughes <hugslondergan@gmail.com>
2019-07-28 17:57:12 -0400

    Add Time Cube

---
+++ server/cards/TimeCube.js
@@ -0,0 +1,21 @@
+var Spell = require('../Spell');
+let Humiliate = require('../genericEffects/Humiliate');
+
+class TimeCube extends Spell {
+
+    constructor() {
+        super(47, 'spell', 'legendary', "Ryrin's Time Cube", 5, false, false, false);
+        this.hasCardPlayed = true;
+        this.addCardPlayed({
+            name: 'Time Cube',
+            func: function(input, game, eventChain) {
+                _.times(game.currentPlayer.board.length, Humiliate.func({targetSide: game.currentPlayer.id, target: index}, game, eventChain));
+
+                _.times(game.otherPlayer.board.length, Humiliate.func({targetSide: game.otherPlayer.id, target: index}, game, eventChain));
+            }
+        });
+    }
+
+}
+
+module.exports = TimeCube;
