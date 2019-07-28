979d4071ddb7f710d2e2a7281a6d03e6c7fc9db0
Hughes <hugslondergan@gmail.com>
2019-07-28 17:36:04 -0400

    Make humiliate kill guys when a hand is full

--- server/genericEffects/Humiliate.js
+++ server/genericEffects/Humiliate.js
@@ -4,19 +4,26 @@ module.exports = {
     name: 'humiliate', 
     func: function(input, game, eventChain) {
 
-        let event = {
-            type: 'humiliate',
-            player: game.currentPlayer,
-            targetSide: input.targetSide,
-            target: input.target
-        };
-
         let player = input.targetSide == 1? game.player1: game.player2;
         let card = player.board.splice(input.target, 1)[0];
 
+        let event;
+
         if(player.hand.length >= constants.MAX_HAND_SIZE) {
+            event = {
+                type: 'kill dead',
+                targetSide: input.targetSide,
+                target: input.target,
+            }
             player.graveyard.push(card);
+            card.currentPower = card.power;
         } else {
+            event = {
+                type: 'humiliate',
+                player: game.currentPlayer,
+                targetSide: input.targetSide,
+                target: input.target
+            };
             player.hand.push(card);
         }
 
