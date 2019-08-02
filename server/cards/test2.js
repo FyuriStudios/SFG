2799277a783946ce48f22510858ce7407e32c67a
Hughes <hugslondergan@gmail.com>
2019-08-01 21:56:18 -0400

    Update test2

--- server/cards/test2.js
+++ server/cards/test2.js
@@ -5,19 +5,14 @@ class test extends Spell {
     constructor() {
         super(-2, 'spell', 'test', 'test', 3, true);
         this.hasCardPlayed = true;
-        this.addCardPlayed(damageWrapper);
+        this.addCardPlayed({
+            name: 'put a name here ya dongis',
+            func: function(input, game, eventChain) {
+                return;
+            }
+        });
     }
 
-}
-
-let damageWrapper = {
-    name: 'damage',
-    
-    func: function(input, game, eventChain) {
-        let damage = require('../genericEffects/Damage');
-
-        damage.func(input, game, eventChain, 4);
-    }
 }
 
 module.exports = test;
