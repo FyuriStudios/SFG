2fdeaf6f11b3d3a16d00683ec618b3abc777b207
Hughes <hugslondergan@gmail.com>
2019-08-01 22:13:09 -0400

    Add a new flag to the Spell class

--- server/Spell.js
+++ server/Spell.js
@@ -2,7 +2,7 @@ let Card = require('./Card');
 
 class Spell extends Card {
 
-    constructor(id, tokenType, rarity, name, cost, targeting, forseeing, field) {
+    constructor(id, tokenType, rarity, name, cost, targeting, forseeing, field, handTargeting = false) {
         super('spell', id, tokenType, rarity, name, cost, targeting, forseeing);
         this.field = field;
     }
