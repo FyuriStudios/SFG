07732d5d151783191fa526ac72beb70ae818520a
Hughes <hugslondergan@gmail.com>
2019-08-13 23:26:48 -0400

    Fix Forsee to cap tokens at max number

--- server/genericEffects/Forsee.js
+++ server/genericEffects/Forsee.js
@@ -1,4 +1,5 @@
 let Effect = require('../Effect');
+let constants = require('../../constants/constants');
 
 module.exports = {
     name: 'Forsee',
@@ -18,7 +19,6 @@ module.exports = {
                         player: game.currentPlayer.id,
                         tokenType: forseeEffect.choice,
                         view: 1,
-                        amount: forseeEffect.amount,
                     };
 
                     eventChain.push(event);
@@ -29,6 +29,28 @@ module.exports = {
                     else if(forseeEffect.choice == 'action') {
                         game.currentPlayer.sToks += forseeEffect.amount;
                     }
+
+                    if(forseeEffect.choice == 'monster') {
+                        if (game.currentPlayer.mToks >= constants.MAX_TOKS - forseeEffect.amount) {
+                            var currMToks = temp.sToks;
+                            game.currentPlayer.mToks = constants.MAX_TOKS;
+                            event.amount = constants.MAX_TOKS - currMToks;
+                        } else {
+                            game.currentPlayer.mToks += forseeEffect.amount;
+                            event.amount = forseeEffect.amount;
+                        }
+                    }
+                    else if(forseeEffect.choice == 'action') {
+                        if (game.currentPlayer.sToks >= constants.MAX_TOKS - forseeEffect.amount) {
+                            var currSToks = temp.sToks;
+                            game.currentPlayer.sToks = constants.MAX_TOKS;
+                            event.amount = constants.MAX_TOKS - currSToks;
+                        } else {
+                            game.currentPlayer.sToks += forseeEffect.amount;
+                            event.amount = forseeEffect.amount;
+                        }
+                    }
+            
                 }
             }
         });
