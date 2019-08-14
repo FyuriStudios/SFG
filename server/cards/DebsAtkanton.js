1108580f4e151905fb40c124e6004e2143dafb76
Hughes <hugslondergan@gmail.com>
2019-08-14 00:01:24 -0400

    Fix Debs

--- server/cards/DebsAtkanton.js
+++ server/cards/DebsAtkanton.js
@@ -6,10 +6,34 @@ class DebsAtkanton extends Monster {
     constructor() {
         super('monster', 47, 'monster', 'common', 'Debs Atkanton', 2, 1, 'advisor', false, false, true, false, true);
         this.hasCardPlayed = true;
-        this.addCardPlayed = ({
+        this.addCardPlayed({
             name: 'Debs Signature',
             func: function(input, game, eventChain){
-                Boost.func(input, game, eventChain, 2);
+
+                if(game.currentPlayer.board.length == 1 && game.otherPlayer.board.length == 0) {
+                    return;
+                }
+
+                let player = game.currentPlayer;
+
+                eventChain.push({
+                    type: 'choose target',
+                    player: player.id,
+                    view: 3
+                });
+
+                player.socket.once('target choice', input => {
+
+                    /*
+                    We have to create this because there is no event chain when the choice is triggered. Therefore,
+                    we have to emit the event ourselves right here. This is gross, I concede that.
+                    */
+                    let newEvent = [];
+                    Boost.func(input, game, newEvent, 2);
+                    game.killDead(newEvent);
+                    game.outputEventChain(newEvent);
+                });
+                
             }
         });
     }
