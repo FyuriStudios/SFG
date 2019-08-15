c942063050504c760b863a5cb610a93a1268810f
Hughes <hugslondergan@gmail.com>
2019-08-15 00:09:30 -0400

    Fix Resourcefulness

--- server/cards/Resourcefulness.js
+++ server/cards/Resourcefulness.js
@@ -4,52 +4,34 @@ let Damage = require('../genericEffects/Damage');
 class Resourcefullness extends Spell {
 
     constructor() {
-        super(58, 'spell', 'common', 'Resourcefullness', 6, false, false, false);
+        super(58, 'spell', 'common', 'Resourcefullness', 6, true, false, false, false);
         this.hasCardPlayed = true;
-        this.addCardPlayed = ({
+        this.addCardPlayed({
             name: 'Resourcefullness',
             func: function(input, game, eventChain){
-                let player = game.currentPlayer;
+                
+                let temp;
+                let amount;
 
-                eventChain.push({
-                    type: 'choose target',
-                    player: player.id,
-                    view: 3
-                });
+                if(input.targetSide == game.currentPlayer.id){
+                    temp = game.currentPlayer.board.splice(input.target, 1)[0];
+                    amount = temp.currentPower;
+                    temp.currentPower = temp.power;
+                    game.currentPlayer.graveyard.push(temp);
 
-                player.socket.once('target choice', input => {
+                    let damageIndex = Math.floor(Math.random() * (game.otherPlayer.board.length + 1) - 1);
+                    Damage.func({
+                        targetSide: game.otherPlayer.id,
+                        target: damageIndex,
+                    }, game, eventChain, amount);
 
-                    /*
-                    We have to create this because there is no event chain when the choice is triggered. Therefore,
-                    we have to emit the event ourselves right here. This is gross, I concede that.
-                    */
-                    let newEvent = [];
-                    let temp;
-                    let amount;
+                    eventChain.push({
+                        type: 'kill dead',
+                        target: input.target,
+                        targetSide: input.targetSide
+                    });
+                }
 
-                    if(input.targetSide == game.currentPlayer.id){
-                        temp = game.currentPlayer.board.splice(input.target, 1)[0];
-                        amount = temp.currentPower;
-                        temp.currentPower = temp.power;
-                        game.currentPlayer.graveyard.push(temp);
-
-                        let damageIndex = Math.floor(Math.random() * (game.otherPlayer.board.length + 1) - 1);
-                        Damage.func({
-                            targetSide: game.otherPlayer.id,
-                            target: damageIndex,
-                        }, game, eventChain, amount);
-                        
-
-                        newEvent.push({
-                            type: 'kill dead',
-                            target: input.target,
-                            targetSide: input.targetSide
-                        });
-                    }
-                    game.outputEventChain(newEvent);
-
-
-                });
             }
         });
     }
