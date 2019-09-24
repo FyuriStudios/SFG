276855c59462417db029146d92f76211c03f84d1
Hughes <hugslondergan@gmail.com>
2019-09-23 20:12:28 -0400

    Rebalance Rotation

--- server/cards/Rotation.js
+++ server/cards/Rotation.js
@@ -4,12 +4,13 @@ let Discard = require('../genericEffects/Discard');
 class Rotation extends Spell {
 
     constructor() {
-        super(56, 'spell', 'common', 'Rotation', 0, false, false, false, true);
+        super(56, 'spell', 'common', 'Rotation', 1, false, false, false, true);
         this.hasCardPlayed = true;
         this.addCardPlayed({
             name: 'Rotation Effect',
             func: function(input, game, eventChain){
                 Discard.func({player: game.currentPlayer.id, index: input.index}, game, eventChain);
+                game.drawCard(game.currentPlayer, eventChain);
                 game.drawCard(game.currentPlayer, eventChain);
             }
         });
